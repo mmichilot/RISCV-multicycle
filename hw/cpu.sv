46c51c96b6220bc498eb180a2ff99cd5a521b895
Matthew Michilot <matthew.michilot@gmail.com>
2021-09-17 18:01:01 -0700

    removed memory and added data bus

--- hw/cpu.sv
+++ hw/cpu.sv
@@ -22,7 +22,13 @@
 module cpu
     (
         input clk,
-        input rst
+        input rst,
+        input error,
+
+        output logic [31:0] addr,
+        
+        output logic [31:0] data_out,
+        input logic [31:0] data_in
     ); 
 
     // -- Signals --
@@ -37,20 +43,9 @@ module cpu
 
     // Datapath
     /* verilator lint_off UNUSED */
-    logic [31:0] addr, inst;
+    logic [31:0] inst;
     /* verilator lint_on UNUSED */
 
-    // Memory
-    logic mem_error;
-    logic [31:0] mem_out, mem_in;
-
-    // -- Deprecated --
-    logic br_lt,br_eq,br_ltu;
-    //Branch Condition Generator
-    always_comb
-    begin
-        br_lt=0; br_eq=0; br_ltu=0;
-    end
 
     (* keep_hierarchy=1 *)
     control_unit control_unit(
@@ -58,7 +53,7 @@ module cpu
     	.clk      (clk      ),
         .rst      (rst      ),
         .opcode   (inst[6:0]),
-        .error    (mem_error),
+        .error    (error),
 
         // Outputs
         .enBranch (enBranch ),
@@ -81,9 +76,6 @@ module cpu
         .func3     (inst[14:12]),
         .func7     (inst[31:25]),
         .aluCtrl   (aluCtrl   ),
-        .CU_BR_EQ  (br_eq  ),
-        .CU_BR_LT  (br_lt ),
-        .CU_BR_LTU (br_ltu ),
 
         // Outputs
         .immedSrc  (immedSrc  ),
@@ -95,7 +87,7 @@ module cpu
         // Inputs
     	.clk      (clk      ),
         .rst      (rst      ),
-        .data_in  (mem_out  ),
+        .data_in  (data_in ),
         .enBranch (enBranch ),
         .pcUpdate (pcUpdate ),
         .irWrite  (irWrite  ),
@@ -110,22 +102,7 @@ module cpu
         // Outputs
         .addr     (addr     ),
         .inst_out (inst ),
-        .data_out (mem_in )
-    );
-
-    (* keep_hierarchy=1 *)
-    memory mem(
-        // Inputs
-    	.clk   (clk   ),
-        .rd    (memRead),
-        .we    (memWrite    ),
-        .addr  (addr  ),
-        .data  (mem_in  ),
-        .size  (inst[13:12]  ),
-
-        // Outputs
-        .out   (mem_out   ),
-        .error (mem_error )
+        .data_out (data_out)
     );
 
 endmodule
