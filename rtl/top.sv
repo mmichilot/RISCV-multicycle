3402f934f9da9d3bf6faa1e4392f7d4661ef13b7
Matthew Michilot <matthew.michilot@gmail.com>
2025-02-07 20:48:16 -0800

    Add ROM and CLINT to top and remove probes

--- rtl/top.sv
+++ rtl/top.sv
@@ -2,22 +2,12 @@
 
 // Top Module
 module top #(
-        SRAM_SIZE = 32_768
+        parameter SRAM_SIZE = 32_768
     ) (
     input clk,
     input rst_n,
 
-    input external_int,
-
-    // scope data memory bus
-    output logic        probe_cyc,
-    output logic        probe_stb,
-    output logic        probe_we,
-    output logic [3:0]  probe_sel,
-    output logic [31:0] probe_adr,
-    output logic [31:0] probe_dat_i,
-    output logic [31:0] probe_dat_o,
-    output logic        probe_ack
+    input external_int
 );
 
     // Core Interrupts
@@ -50,56 +40,90 @@ module top #(
 
         .interrupts
     );
-    
+
+    localparam ROM_BASE = 32'h0000_0000;
+    localparam ROM_MASK = 32'hFFFF_FE00;
+    localparam SRAM_BASE = 32'h8000_0000;
+    localparam SRAM_MASK = ~32'(SRAM_SIZE-1);
+    localparam CLINT_BASE = 32'h0200_0000;
+    localparam CLINT_MASK = 32'hFFFF_0000;
+
+    logic rom_ack_o;
+    logic [31:0] rom_dat_o;
+    wb_rom rom(
+        .wb_cyc_i (wb_cyc_o),
+        .wb_stb_i (wb_stb_o & rom_sel),
+        .wb_ack_o (rom_ack_o),
+        .wb_adr_i (wb_adr_o[10:0]),
+        .wb_dat_o (rom_dat_o)
+    );
+
+
     localparam SRAM_ADDR_WIDTH = $clog2(SRAM_SIZE);
-    logic sram_stb_i, sram_ack_o;
+    logic sram_ack_o;
     logic [31:0] sram_dat_o;
-    assign sram_stb_i = dmem_stb_o & sram_sel;
-    assign dmem_dat_i = sram_dat_o;
-    assign dmem_ack_i = sram_ack_o;
     wb_sram #(
         .SIZE_BYTES(SRAM_SIZE)
     ) sram (
-        .a_clk_i (clk),
-        .a_cyc_i (imem_cyc_o),
-        .a_stb_i (imem_stb_o),
-        .a_adr_i (imem_adr_o[SRAM_ADDR_WIDTH-1:0]),
-        .a_we_i  (imem_we_o),
-        .a_sel_i (imem_sel_o),
-        .a_dat_i (imem_dat_o),
-        .a_dat_o (imem_dat_i),
-        .a_ack_o (imem_ack_i),
-
-        .b_clk_i (clk),
-        .b_cyc_i (dmem_cyc_o),
-        .b_stb_i (sram_stb_i),
-        .b_adr_i (dmem_adr_o[SRAM_ADDR_WIDTH-1:0]),
-        .b_we_i  (dmem_we_o),
-        .b_sel_i (dmem_sel_o),
-        .b_dat_i (dmem_dat_o),
-        .b_dat_o (sram_dat_o),
-        .b_ack_o (sram_ack_o)
+        .wb_clk_i (clk),
+        .wb_cyc_i (wb_cyc_o),
+        .wb_stb_i (wb_stb_o & sram_sel),
+        .wb_adr_i (wb_adr_o[SRAM_ADDR_WIDTH-1:0]),
+        .wb_we_i  (wb_we_o),
+        .wb_sel_i (wb_sel_o),
+        .wb_dat_i (wb_dat_o),
+        .wb_dat_o (sram_dat_o),
+        .wb_ack_o (sram_ack_o)
     );
 
-    logic sram_sel, slv1_sel;
-    localparam SRAM_MASK = ~32'(SRAM_SIZE-1);
+    logic clint_ack_o;
+    logic [31:0] clint_dat_o;
+    wb_clint clint (
+        .clk ,
+        .rst_n,
+
+        .wb_cyc_i (wb_cyc_o),
+        .wb_stb_i (wb_stb_o & clint_sel),
+        .wb_we_i  (wb_we_o),
+        .wb_adr_i (wb_adr_o[15:0]),
+        .wb_sel_i (wb_sel_o),
+        .wb_dat_i (wb_dat_o),
+        .wb_dat_o (clint_dat_o),
+        .wb_ack_o (clint_ack_o),
+
+        .software_int,
+        .timer_int
+    );
+
+
+    logic rom_sel, sram_sel, clint_sel;
     wb_decoder wb_decoder(
-        .adr_i           (dmem_adr_o),
-        .slv0_adr_prefix (32'h8000_0000),
-        .slv0_adr_mask   (SRAM_MASK),
-        .acmp0           (sram_sel),
-        .slv1_adr_prefix (32'h0000_0000),
-        .slv1_adr_mask   (32'hFFFF_FFFF),
-        .acmp1           (slv1_sel)
+        .adr_i           (wb_adr_o),
+
+        .slv0_adr_prefix (ROM_BASE),
+        .slv0_adr_mask   (ROM_MASK),
+        .acmp0           (rom_sel),
+
+        .slv1_adr_prefix (SRAM_BASE),
+        .slv1_adr_mask   (SRAM_MASK),
+        .acmp1           (sram_sel),
+
+        .slv2_adr_prefix (CLINT_BASE),
+        .slv2_adr_mask   (CLINT_MASK),
+        .acmp2           (clint_sel)
     );
-    
-    assign probe_cyc    = dmem_cyc_o;
-    assign probe_stb    = dmem_stb_o;
-    assign probe_we     = dmem_we_o;
-    assign probe_sel    = dmem_sel_o;
-    assign probe_adr    = dmem_adr_o;
-    assign probe_dat_i  = dmem_dat_i;
-    assign probe_dat_o  = dmem_dat_o;
-    assign probe_ack    = dmem_ack_i;
+
+    always_comb begin : wb_dat_i_mux
+        wb_dat_i = '0;
+
+        if ((wb_adr_o & ROM_MASK) == ROM_BASE)
+            wb_dat_i = rom_dat_o;
+        if ((wb_adr_o & SRAM_MASK) == SRAM_BASE)
+            wb_dat_i = sram_dat_o;
+        if ((wb_adr_o & CLINT_MASK) == CLINT_BASE)
+            wb_dat_i = clint_dat_o;
+    end
+
+    assign wb_ack_i = clint_ack_o | sram_ack_o | rom_ack_o;
 
 endmodule
