e5841a1407ee75016633e3d76ac5f71f3423c464
Matthew Michilot <matthew.michilot@gmail.com>
2025-02-11 15:03:11 -0800

    Add RESET_ADDR parameter to prog_cntr

--- rtl/core/prog_cntr.sv
+++ rtl/core/prog_cntr.sv
@@ -1,17 +1,19 @@
 `timescale 1ns / 1ps
 
-module prog_cntr(
-    input clk,
-    input rst_n,
-    input ld,
-    input logic [31:0] data,
-    output logic [31:0] count
+module prog_cntr #(
+        parameter RESET_ADDR = 32'h8000_0000
+    )(
+        input clk,
+        input rst_n,
+        input ld,
+        input logic [31:0] data,
+        output logic [31:0] count
     );
 
     always_ff @(posedge clk or negedge rst_n)
     begin
         if (!rst_n)
-            count <= '0;
+            count <= RESET_ADDR;
         else if (ld)
             count <= data;
     end
